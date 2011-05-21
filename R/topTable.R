86283f75eca75c27b4ed477ca1a544fcc39eb390
Tobias Verbeke <tobias.verbeke@openanalytics.eu>
2011-05-21 14:07:21 +0000

    update for glmnet changes
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/a4Core@55766 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/topTable.R
+++ R/topTable.R
@@ -3,6 +3,7 @@ setGeneric("topTable", function(fit, n, ...){ # common to nlcv and (at least) a4
     })
 
 setOldClass("glmnet")
+setOldClass("lognet") # from glmnet 1.5.3 onwards
 
 setMethod("topTable",
     "glmnet",
@@ -28,7 +29,36 @@ setMethod("topTable",
     }
 )
 
+setMethod("topTable",
+    "lognet",
+    function(fit, n){
+      summary.output <- summary(fit)
+      coef.output <- coef(fit) # extract coefficients at a single value of lambda
+      last.coef.output <- coef.output[, ncol(coef.output)]
+      selProbeSets <- last.coef.output[which(last.coef.output != 0)]
+      selProbeSetsGeneSymbol <- fit$featureData[names(selProbeSets), "SYMBOL"]
+      
+      selGenesOutput <- cbind.data.frame(selProbeSetsGeneSymbol, selProbeSets)
+      rownames(selGenesOutput) <- names(selProbeSets)
+      colnames(selGenesOutput) <- c('Gene','Coefficient')
+      # remove the estimate of the intercept (typically, but not always, the first row)
+      exclIntercept <- which(rownames(selGenesOutput)%in%c('','(Intercept)'))
+      selGenesOutput <- selGenesOutput[-exclIntercept,]
+      
+      numberSelGenes <- nrow(selGenesOutput)
+      topList <- selGenesOutput[order(abs(selGenesOutput[,2]),decreasing=TRUE),][1:(min(n, numberSelGenes)),] # first row is the estimate of the intercept.
+      res <- list(topList = topList, numberSelGenes = numberSelGenes, n = n)
+      class(res) <- "topTableLognet"
+      return(res)
+    }
+)
+
 print.topTableGlmnet <- function(x,  ...){
+  cat("The lasso selected ", x$numberSelGenes, " genes. The top ", x$n, " genes are:\n\n", sep = "")
+  print(x$topList, ...)
+}
+
+print.topTableLognet <- function(x,  ...){
   cat("The lasso selected ", x$numberSelGenes, " genes. The top ", x$n, " genes are:\n\n", sep = "")
   print(x$topList, ...)
 }
