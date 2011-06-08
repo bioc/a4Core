31ed86cc1a847982795abf454689ca8ee132c97f
Tobias Verbeke <tobias.verbeke@openanalytics.eu>
2011-06-08 09:22:37 +0000

    make compatible with latest glmnet
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_8/madman/Rpacks/a4Core@56036 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/topTable.R
+++ R/topTable.R
@@ -3,33 +3,68 @@ setGeneric("topTable", function(fit, n, ...){ # common to nlcv and (at least) a4
     })
 
 setOldClass("glmnet")
+setOldClass("elnet")
+setOldClass("lognet") # from glmnet 1.5.3 onwards
+
+glmnetUtil <- function(fit, n){
+  summary.output <- summary(fit)
+  coef.output <- coef(fit) # extract coefficients at a single value of lambda
+  last.coef.output <- coef.output[, ncol(coef.output)]
+  selProbeSets <- last.coef.output[which(last.coef.output != 0)]
+  selProbeSetsGeneSymbol <- fit$featureData[names(selProbeSets), "SYMBOL"]
+  
+  selGenesOutput <- cbind.data.frame(selProbeSetsGeneSymbol, selProbeSets)
+  rownames(selGenesOutput) <- names(selProbeSets)
+  colnames(selGenesOutput) <- c('Gene','Coefficient')
+  # remove the estimate of the intercept (typically, but not always, the first row)
+  exclIntercept <- which(rownames(selGenesOutput)%in%c('','(Intercept)'))
+  selGenesOutput <- selGenesOutput[-exclIntercept,]
+  
+  numberSelGenes <- nrow(selGenesOutput)
+  topList <- selGenesOutput[order(abs(selGenesOutput[,2]),decreasing=TRUE),][1:(min(n, numberSelGenes)),] # first row is the estimate of the intercept.
+  retval <- list(topList = topList, numberSelGenes = numberSelGenes, n = n)
+  return(retval)
+}
 
 setMethod("topTable",
     "glmnet",
     function(fit, n){
-      summary.output <- summary(fit)
-      coef.output <- coef(fit) # extract coefficients at a single value of lambda
-      last.coef.output <- coef.output[, ncol(coef.output)]
-      selProbeSets <- last.coef.output[which(last.coef.output != 0)]
-      selProbeSetsGeneSymbol <- fit$featureData[names(selProbeSets), "SYMBOL"]
-      
-      selGenesOutput <- cbind.data.frame(selProbeSetsGeneSymbol, selProbeSets)
-      rownames(selGenesOutput) <- names(selProbeSets)
-      colnames(selGenesOutput) <- c('Gene','Coefficient')
-      # remove the estimate of the intercept (typically, but not always, the first row)
-      exclIntercept <- which(rownames(selGenesOutput)%in%c('','(Intercept)'))
-      selGenesOutput <- selGenesOutput[-exclIntercept,]
-      
-      numberSelGenes <- nrow(selGenesOutput)
-      topList <- selGenesOutput[order(abs(selGenesOutput[,2]),decreasing=TRUE),][1:(min(n, numberSelGenes)),] # first row is the estimate of the intercept.
-      res <- list(topList = topList, numberSelGenes = numberSelGenes, n = n)
+      res <- glmnetUtil(fit = fit, n = n)
       class(res) <- "topTableGlmnet"
       return(res)
     }
 )
+
+setMethod("topTable",
+    "lognet",
+    function(fit, n){
+      res <- glmnetUtil(fit = fit, n = n)
+      class(res) <- "topTableLognet"
+      return(res)
+    }
+)
+
+setMethod("topTable",
+    "elnet",
+    function(fit, n){
+      res <- glmnetUtil(fit = fit, n = n)
+      class(res) <- "topTableElnet"
+      return(res)
+    }
+)
+
 
 print.topTableGlmnet <- function(x,  ...){
   cat("The lasso selected ", x$numberSelGenes, " genes. The top ", x$n, " genes are:\n\n", sep = "")
   print(x$topList, ...)
 }
 
+print.topTableLognet <- function(x,  ...){
+  cat("The lasso selected ", x$numberSelGenes, " genes. The top ", x$n, " genes are:\n\n", sep = "")
+  print(x$topList, ...)
+}
+
+print.topTableElnet <- function(x,  ...){
+  cat("The lasso selected ", x$numberSelGenes, " genes. The top ", x$n, " genes are:\n\n", sep = "")
+  print(x$topList, ...)
+}
